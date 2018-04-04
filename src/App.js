1391c1dbf4ff10542ed88686b36c6d02bd76b02d
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-04-04 17:06:51 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return <div>Hello World</div>
+  }
+}
+
+export default App
