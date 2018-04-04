84a778914bc7b5d2bb9a8c1660a704e002318416
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-04-05 00:11:35 +0200

    State Management | Fetch & delete contacts on the server

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
 import React, { Component } from 'react'
 import ContactList from './components/ContactList'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        id: 'karen',
-        name: 'Karen Isgrigg',
-        handle: 'karen_isgrigg',
-        avatarURL: 'http://localhost:5001/karen.jpg'
-      },
-      {
-        id: 'richard',
-        name: 'Richard Kalehoff',
-        handle: 'richardkalehoff',
-        avatarURL: 'http://localhost:5001/richard.jpg'
-      },
-      {
-        id: 'tyler',
-        name: 'Tyler McGinnis',
-        handle: 'tylermcginnis',
-        avatarURL: 'http://localhost:5001/tyler.jpg'
-      }
-    ]
+    contacts: []
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll().then(contacts => {
+      this.setState(() => ({ contacts }))
+    })
   }
 
   deleteContact = contact => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(c => c.id !== contact.id)
     }))
+
+    ContactsAPI.remove(contact)
   }
 
   render() {
