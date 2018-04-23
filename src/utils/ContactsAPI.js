f8554544368c9278982e547032f2ffeaf729abe0
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-04-23 14:00:24 +0200

    Change api to heroku

--- src/utils/ContactsAPI.js
+++ src/utils/ContactsAPI.js
@@ -1,34 +1,36 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5001'
+const api =
+  process.env.REACT_APP_CONTACTS_API_URL ||
+  "https://evening-mountain-37762.herokuapp.com";
 
-let { token } = localStorage
+let { token } = localStorage;
 
 if (!token)
   // eslint-disable-next-line no-multi-assign
   token = localStorage.token = Math.random()
     .toString(36)
-    .substr(-8)
+    .substr(-8);
 
 const headers = {
-  Accept: 'application/json',
+  Accept: "application/json",
   Authorization: token
-}
+};
 
 export const getAll = () =>
   fetch(`${api}/contacts`, { headers })
     .then(res => res.json())
-    .then(data => data.contacts)
+    .then(data => data.contacts);
 
 export const remove = contact =>
-  fetch(`${api}/contacts/${contact.id}`, { method: 'DELETE', headers })
+  fetch(`${api}/contacts/${contact.id}`, { method: "DELETE", headers })
     .then(res => res.json())
-    .then(data => data.contact)
+    .then(data => data.contact);
 
 export const create = body =>
   fetch(`${api}/contacts`, {
-    method: 'POST',
+    method: "POST",
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(body)
-  }).then(res => res.json())
+  }).then(res => res.json());
